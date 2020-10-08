178a8af8db755e33b33e05da7e4ef351f87555e7
Michael Thomas <michaelhthomas@outlook.com>
2020-10-08 12:29:03 -0400

    Update fb conf and rm money-we-dont-have functions

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
 import './registerServiceWorker'
+
+import * as firebase from "firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBolpUsmq6o0f4En4k3dHcR3ExRMna2vZM",
+  authDomain: "river-bluff-high-school.firebaseapp.com",
+  databaseURL: "https://river-bluff-high-school.firebaseio.com",
+  projectId: "river-bluff-high-school",
+  storageBucket: "river-bluff-high-school.appspot.com",
+  messagingSenderId: "208923448537",
+  appId: "1:208923448537:web:8bbadd5e5791562dc95ac8"
+}
+
+firebase.initializeApp(firebaseConfig)
+
+// firebase.auth().onAuthStateChanged(user => {
+//   store.dispatch("fetchUser", user)
+// })
 
 Vue.config.productionTip = false
 
