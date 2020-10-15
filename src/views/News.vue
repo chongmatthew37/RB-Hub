031894a926b02f0d5ef98517111b89bd746184eb
Michael Thomas <michaelhthomas@outlook.com>
2020-10-15 14:40:30 -0400

    Basic news with rss

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,17 +1,61 @@
 <template>
     <div class="news">
-        <NewsCarousel />
-        <h1>Later Gator Daily Announcements - {{ new Date() | moment("MMMM DD") }}</h1>
+        <v-carousel>
+          <v-carousel-item
+            v-for="item in carousel"
+            :key="item"
+            :src="item.thumbnail"
+            cycle=true
+          >
+          </v-carousel-item>
+        </v-carousel>
+        <v-card 
+          v-for="item in news"
+          :key="item.pubDate"
+          class="my-5"
+        >
+          <v-img
+            v-if="item.thumbnail"
+            :src="item.thumbnail"
+          ></v-img>
+          <v-card-title>{{ item.title }}</v-card-title>
+          <v-card-text v-html="item.description.replace(/<img[^>]*>/g,'')"></v-card-text>
+        </v-card>
     </div>
 </template>
 
 <script>
-import NewsCarousel from '@/components/NewsCarousel.vue'
-
 export default {
   name: 'News',
-  components: {
-    NewsCarousel
+  data: () => {
+    return ({
+      news: [],
+      carousel: []
+    })
+  },
+  methods: {
+    sortNews: async function () {
+      let response = await fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Friverbluffathletics.com%2Ffeed%2F")
+      let json = await response.json()
+
+      json.items.forEach(item => {
+        if (item.description.replace(/<img[^>]*>/g,'') != "<p></p>") {
+          this.news.push(item);
+        } else if (item.thumbnail != "") {
+          this.carousel.push(item);
+        }
+      });
+    }
+  },
+  created: function () {
+    this.sortNews();
   }
 }
 </script>
+
+<style scoped>
+img {
+  max-width: 100%!important;
+  height: auto!important;
+}
+</style>
